cadb5888d1b01677079e60b3a01b9d3d8ab2c8d3
Julio Quezada <julio.quezada@catolica.edu.sv>
2022-10-03 17:33:39 -0600

    Update

--- script.js
+++ script.js
@@ -26,6 +26,42 @@ window.addEventListener('load', function(){
             });
         }
     }
+    class SoundController {
+        constructor(){
+            this.powerUpSound = document.getElementById('powerup');
+            this.powerDownSound = document.getElementById('powerdown');
+            this.explosionSound = document.getElementById('explosion');
+            this.shotSound = document.getElementById('shot');
+            this.hitSound = document.getElementById('hit');
+            this.shieldSound = document.getElementById('shield');
+
+        }
+        powerUp(){
+            this.powerUpSound.currentTime = 0;
+            this.powerUpSound.play();
+        }
+        powerDown(){
+            this.powerDownSound.currentTime = 0;
+            this.powerDownSound.play();
+        }
+        explosion(){
+            this.explosionSound.currentTime = 0;
+            this.explosionSound.play();
+        }
+        shot(){
+            this.shotSound.currentTime = 0;
+            this.shotSound.play();
+        }
+        hit(){
+            this.hitSound.currentTime = 0;
+            this.hitSound.play();
+        }
+        shield(){
+            this.shieldSound.currentTime = 0;
+            this.shieldSound.play();
+        }
+
+    }
     class Projectile {
         constructor(game, x, y){
             this.game = game;
@@ -128,6 +164,7 @@ window.addEventListener('load', function(){
                     this.powerUpTimer = 0;
                     this.powerUp = false;
                     this.frameY = 0;
+                    this.game.sound.powerDown();
                 } else {
                     this.powerUpTimer += deltaTime;
                     this.frameY = 1;
@@ -159,6 +196,7 @@ window.addEventListener('load', function(){
             this.powerUpTimer = 0;
             this.powerUp = true;
             if (this.game.ammo < this.game.maxAmmo) this.game.ammo = this.game.maxAmmo;
+            this.game.sound.powerUp();
         }
     }
 
@@ -421,6 +459,7 @@ window.addEventListener('load', function(){
             this.player = new Player(this);
             this.input = new InputHandler(this);
             this.ui = new UI(this);
+            this.sound = new SoundController();
             this.keys = [];
             this.enemies = [];
             this.particles = [];
